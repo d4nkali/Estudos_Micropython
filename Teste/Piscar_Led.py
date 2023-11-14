3da4808942d34b93fe8722d3962a0e8f8e1d8633
d4nkali <133923419+d4nkali@users.noreply.github.com>
2023-11-14 13:26:29 -0300

    Update

--- Teste/Piscar_Led.py
+++ Teste/Piscar_Led.py
@@ -1,11 +1,11 @@
-# Piscar led no MicroPython
+# Programa Piscar led
 
 # Importa as bibliotecas
 import machine
 import time
 
-led = machine.Pin(2, machine.Pin.OUT)  # Define o GPIO2 como o pino do LED da placa em sada
+led = machine.Pin(2, machine.Pin.OUT)  # Define o pino GPIO2 como do LED da placa
 
-while True: # Loop
-    led.value(not led.value()) # Liga e desliga o led
+while True: # Cria um loop
+    led.value(not led.value()) # Alterna os valores entre ligado e desligado
     time.sleep(1) # Aguarda 1 segundo
