50ad4a68a60fd1e6e29fc052aa96d69092699d1b
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-01-16 15:20:33 -0300

    Update

--- Teste/Teste_Wifi.py
+++ Teste/Teste_Wifi.py
@@ -3,12 +3,14 @@
 # Importa a biblioteca
 import network
 
+ssid = 'Seu_wifi' # Substituir a variavel SSID pelo nome do wifi da sua preferencia
+senha = 'Senha_do_seu_wifi' # Substituir a variavel Senha para a senha do wifi da sua preferencia
 sta_if = network.WLAN(network.STA_IF) # Cria uma variavel de configurao da biblioteca
 
 if not sta_if.isconnected(): # Se o wifi no conectar, ento:
-    print('connecting to network...') # Imprime a mensagen de conectar a rede
+    print('Conectando ao WiFi...') # Imprime a mensagen de conectar a rede
     sta_if.active(True) # Ativa o wifi
-    sta_if.connect('SSID', 'Senha') # Conecta ao wifi onde substituir o SSID pelo nome do wifi e a senha para o wifi da sua preferencia
+    sta_if.connect(ssid, senha) # Conecta ao wifi 
     while not sta_if.isconnected(): # Se no conectar
         pass # Cria o loop at conectar a rede
-print('network config:', sta_if.ifconfig()) # Imprime as informaes da rede
+print('Informaes da Rede: ', sta_if.ifconfig()) # Imprime as informaes da rede
